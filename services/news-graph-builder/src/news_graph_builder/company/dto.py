e18cd2ef60e626dbb0603a93965801f2233565f9
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2026-09-26 11:39:47 +0900

    chore: Add comments to DartCompany fields for clarity
    Added comments to clarify the purpose of 'corp_code' and 'stock_code'.

--- services/news-graph-builder/src/news_graph_builder/company/dto.py
+++ services/news-graph-builder/src/news_graph_builder/company/dto.py
@@ -2,7 +2,7 @@ from typing import NamedTuple
 
 
 class DartCompany(NamedTuple):
-    corp_code: str
+    corp_code: str            # 8 digits ID from OpenDART
     corp_name: str
     corp_eng_name: str | None
-    stock_code: str
+    stock_code: str           # 6 alphanumeric ID
